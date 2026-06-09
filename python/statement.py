36f101a501d30c8c467529315081770f3dd09f6f
aledizba7 <alediaz709@gmail.com>
2026-06-08 20:03:00 -0600

    refactor: extract calculate_amount function

--- python/statement.py
+++ python/statement.py
@@ -8,6 +8,21 @@ def calculate_credits(perf, play):
     return result
 
 
+def amount_for(perf, play):
+    if play['type'] == "tragedy":
+        result = 40000
+        if perf['audience'] > 30:
+            result += 1000 * (perf['audience'] - 30)
+    elif play['type'] == "comedy":
+        result = 30000
+        if perf['audience'] > 20:
+            result += 10000 + 500 * (perf['audience'] - 20)
+        result += 300 * perf['audience']
+    else:
+        raise ValueError(f'unknown type: {play["type"]}')
+    return result
+
+
 def statement(invoice, plays):
     total_amount = 0
     volume_credits = 0
@@ -18,19 +33,7 @@ def statement(invoice, plays):
 
     for perf in invoice['performances']:
         play = plays[perf['playID']]
-        if play['type'] == "tragedy":
-            this_amount = 40000
-            if perf['audience'] > 30:
-                this_amount += 1000 * (perf['audience'] - 30)
-        elif play['type'] == "comedy":
-            this_amount = 30000
-            if perf['audience'] > 20:
-                this_amount += 10000 + 500 * (perf['audience'] - 20)
-
-            this_amount += 300 * perf['audience']
-
-        else:
-            raise ValueError(f'unknown type: {play["type"]}')
+        this_amount = amount_for(perf, play)
 
         # add volume credits
         volume_credits += calculate_credits(perf, play)
